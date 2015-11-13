9154f9e6252f4833a88e834686be9d484f547b1f
Matt Redinger <mattred1@gmail.com>
2015-11-12 21:04:36 -0500

    Add JS just for fun

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+$('.cool').addClass('.hot');
+$('li:lt(4)').hide().fadeIn(3000);
+$('li').on('load', function() {
+  $(this).remove();
+});
 
+$('.hot').removeClass('.cool');
+$('li:lt(4)').show()
+$('li').on('click', function() {
+  $(this).remove();
+});
