a383ffd82bac6ec2e5d3170f9337434d6f72560f
Matt Redinger <mattred1@gmail.com>
2015-11-17 20:17:41 -0500

    Playing around

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-$('.cool').addClass('.hot');
-$('li:lt(4)').hide().fadeIn(3000);
-$('li').on('load', function() {
-  $(this).remove();
-});
+// $('.cool').addClass('.hot');
+// $('li:lt(4)').hide().fadeIn(3000);
+// $('li').on('load', function() {
+//   $(this).remove();
+// });
+//
+// $('.hot').removeClass('.cool');
+// $('li:lt(4)').show()
+// $('li').on('click', function() {
+//   $(this).remove();
+// });
 
-$('.hot').removeClass('.cool');
-$('li:lt(4)').show()
-$('li').on('click', function() {
-  $(this).remove();
-});
+document.getElementById("demo").innerHTML = "Hello Dolly.";
